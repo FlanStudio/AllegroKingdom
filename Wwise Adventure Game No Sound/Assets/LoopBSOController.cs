2fcf2793eea3fff577af6b47a361b7a25d9ddd32
Jony635 <jony635molina@gmail.com>
2019-06-08 13:39:42 +0200

    unnecesary code

--- Wwise Adventure Game No Sound/Assets/LoopBSOController.cs
+++ Wwise Adventure Game No Sound/Assets/LoopBSOController.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class LoopBSOController : MonoBehaviour {
 
     private AudioSource source;
-    private float startTime;
 
     public float preEntrySeconds = 0f;
     public float preExitSeconds = 16f;
@@ -14,7 +13,6 @@ public class LoopBSOController : MonoBehaviour {
 	void Start ()
     {
         source = GetComponent<AudioSource>();
-        startTime = Time.time;
 	}
 	
 	// Update is called once per frame
