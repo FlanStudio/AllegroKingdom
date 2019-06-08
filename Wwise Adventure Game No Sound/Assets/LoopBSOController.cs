d4a18440abcf46c17834c35eb205ab86305d2f2f
Jony635 <jony635molina@gmail.com>
2019-06-08 13:38:27 +0200

    my part finished

---
+++ Wwise Adventure Game No Sound/Assets/LoopBSOController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopBSOController : MonoBehaviour {
+
+    private AudioSource source;
+    private float startTime;
+
+    public float preEntrySeconds = 0f;
+    public float preExitSeconds = 16f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        source = GetComponent<AudioSource>();
+        startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (source.time > preExitSeconds)
+            source.time = preEntrySeconds;
+	}
+}
