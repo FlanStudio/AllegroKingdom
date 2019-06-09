462819ce1ccee668d98b71e76f26bf4e2e9f2871
Óscar Hernández <oscarhernandezgomez@gmail.com>
2019-06-09 16:57:30 +0200

    QuestRoll sound

--- Wwise Adventure Game No Sound/Assets/Scripts/Object Utility/SoundOnAnimationEvent.cs
+++ Wwise Adventure Game No Sound/Assets/Scripts/Object Utility/SoundOnAnimationEvent.cs
@@ -8,13 +8,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundOnAnimationEvent : MonoBehaviour {
+public class SoundOnAnimationEvent : MonoBehaviour
+{
 
     // HINT: Expose the sound to be played in this animation
     //public List<AK.Wwise.Event> Sounds = new List<AK.Wwise.Event>();
+    [Header("Audio")]
+    public AudioSource audioSource;
+    public AudioClip showRoll;
+    public AudioClip hideRoll;
 
-    public void PlaySoundWithIdx(int idx){
+    public void PlaySoundWithIdx(int idx)
+    {
         // HINT: Play sound with index idx synchronized with animation
         //Sounds[idx].Post(gameObject);
+
+        if (idx == 0)
+        {
+            audioSource.clip = showRoll;
+            audioSource.Play();
+        }
+
+        else if (idx == 2)
+        {
+            audioSource.clip = hideRoll;
+            audioSource.Play();
+        }
+
     }
 }
